1b80a658394c978f7f85fc78a20b624e9219a527
tuhincse08 <tuhin.cse08@gmail.com>
2023-10-12 18:51:33 +0600

    add event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Home() {
+    const handleClick = (e) => console.log("Button Clicked",e);
+
+    const handleClickAgain = (name,e)=>{
+        console.log("Hello " + name ,e.target);
+    }
   return (
     <div className="home">
         <h2>HomePage</h2>
+        <button onClick={handleClick}> Click Me</button>
+        <button onClick={(e)=> handleClickAgain('Mario',e)}>Click Me Agian</button>
     </div>
   )
 }
