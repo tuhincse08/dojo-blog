365fc4295743bb0df6193a0a11fe46bdbba09375
tuhincse08 <tuhin.cse08@gmail.com>
2023-10-13 03:02:06 +0600

    condition & Error handling

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,32 @@ import BlogList from './BlogList';
 export default function Home() {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then(res =>{ return res.json();}) 
-        .then(data => {setBlogs(data);})
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+            .then(res =>{ 
+                if(!res.ok) throw Error('Could not fetch data');
+                return res.json();}) 
+            .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+            })
+            .catch(err=>{
+                setError(err.message);
+                setIsPending(false);
+            })
+        }, 1000)
        
     },[])
    
   return (
     <div className="home">
+        {error && <div>{error}</div> }
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
     </div>
   )
